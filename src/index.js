88d5d8ac9f6eaa05b90b3a990f936ed6f6a07173
pravinpoudel <prvnpoudel4@gmail.com>
2023-01-13 17:09:32 -0700

    minimap aded

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import * as THREE from "three";
 import { Octree, Box, Point } from "./octree";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { CopyShader } from "three/examples/jsm/shaders/CopyShader";
+import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader";
 import Stats from "three/examples/jsm/libs/stats.module";
+
 import "./styles/main.css";
 import { fillArray, fillMidNodes } from "./helper";
 let camera, scene, renderer;
@@ -11,8 +17,34 @@ let scene_width = 1000;
 let scene_height = 1000;
 let scene_depth = 1000;
 let qt;
+let mapCamera,
+  mapSizeX = 128,
+  mapSizeY = 64;
+
+let _width = window.innerWidth;
+let _height = window.innerHeight;
+let right = 1024,
+  left = -1024,
+  top = 1024,
+  bottom = -1024;
+
+let composerScreen;
+let composerMap;
+
 scene = new THREE.Scene();
+scene.background = new THREE.Color(0xcccccc);
+let parameters = {
+  minFilter: THREE.LinearFilter,
+  magFilter: THREE.LinearFilter,
+  format: THREE.RGBAFormat,
+  stencilBuffer: false,
+  type: THREE.FloatType,
+};
+
+let renderTarget = new THREE.WebGLRenderTarget(256, 256, parameters);
+
 let points = [];
+
 init();
 function init() {
   camera = new THREE.PerspectiveCamera(
@@ -34,11 +66,12 @@ function init() {
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+  renderer.autoClear = false;
 
   controls = new OrbitControls(camera, renderer.domElement);
   controls.listenToKeyEvents(window); // optional
 
-  fillArray(points, 10000, scene_width, scene_height, scene_depth);
+  fillArray(points, 3000, scene_width, scene_height, scene_depth);
   // points.forEach((element, index) => {
   //   // scene.add(element.mesh);
   // });
@@ -91,6 +124,47 @@ function findLevel(qt) {
   }
   return traverseTree();
 }
+
+function initMapCamera() {
+  mapCamera = new THREE.OrthographicCamera(left, right, top, bottom, 0.1, 1000);
+  // for camera to see down up should be on z axis
+  mapCamera.up = new THREE.Vector3(0, 0, 1);
+  mapCamera.lookAt(0, -1, 0);
+  mapCamera.position.set(0, 0, 0);
+  mapCamera.position.y = 1200;
+  scene.add(mapCamera);
+  // const helper = new THREE.CameraHelper(mapCamera)
+  // scene.add(helper)
+}
+
+function postProcessing() {
+  let pixelRatio = 1;
+  composerScreen = new EffectComposer(renderer);
+  const renderPass = new RenderPass(scene, camera);
+  let fxaaPass = new ShaderPass(FXAAShader);
+  fxaaPass.material.uniforms["resolution"].value.x =
+    1 / (window.innerWidth * pixelRatio);
+  fxaaPass.material.uniforms["resolution"].value.y =
+    1 / (window.innerHeight * pixelRatio);
+  const copyPass1 = new ShaderPass(CopyShader);
+  copyPass1.renderToScreen = true;
+  composerScreen.addPass(renderPass);
+  composerScreen.addPass(fxaaPass);
+  composerScreen.addPass(copyPass1);
+
+  composerMap = new EffectComposer(renderer, renderTarget);
+  composerMap.setSize(256, 256);
+  let renderPassMap = new RenderPass(scene, mapCamera);
+  composerMap.addPass(renderPassMap);
+  var effectFXAA_Map = new ShaderPass(FXAAShader);
+  effectFXAA_Map.uniforms["resolution"].value.set(1 / 256, 1 / 256);
+  composerMap.addPass(effectFXAA_Map);
+  // composerMap.addPass(gammaCorrectionPass);
+  const copyPass = new ShaderPass(CopyShader);
+  copyPass.renderToScreen = true;
+  composerMap.addPass(copyPass);
+}
+
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -111,22 +185,35 @@ points.forEach((element, index) => {
     count++;
   }
 });
+let visiblePoints = [];
 controls.addEventListener("change", () => {
-  findLevel(qt);
+  visiblePoints = findLevel(qt);
+  visiblePoints.forEach((element, index) => {
+    // console.log("aded");
+    boxGroup.add(points[element].mesh);
+  });
 });
 
 const stats_mb = Stats();
 stats_mb.domElement.style.cssText = "position:absolute;top:50px;right:50px;";
 stats_mb.showPanel(2);
 document.body.appendChild(stats_mb.dom);
-function animate() {
+initMapCamera();
+postProcessing();
+function animate(delta) {
+  delta = Math.max(delta, 0.1);
   requestAnimationFrame(animate);
   stats_mb.update();
   controls.autoRotate = true;
   controls.update();
-  renderer.render(scene, camera);
+  renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
+  composerScreen.render(delta);
+  renderer.clear(false, true, false);
+  renderer.setViewport(20, 50, 256, 256);
+  composerMap.render(delta);
+  // renderer.render(scene, camera);
 }
 
-animate();
+window.requestAnimationFrame(animate);
 
 export { scene, scene_width, scene_height, scene_depth };
